781bf2144580c1c2874d694f9e13f00782514950
eduardaguardiao <maria.guardiao@sptech.school>
2024-10-02 11:41:42 -0300

    ADD: Adicionando tela de login

---
+++ nexus-frontend/src/interface-professor/componentes/ButtonLoginGoogle/ButtonLoginGoogle.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './ButtonLoginGoogle.css'
+import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
+
+
+
+
+export default function ButtonLoginGoogle() {
+    return (
+
+        <>
+            <button className="bt-google-login">
+                <span className="span-google">
+                    <img src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo" />
+                    Google
+                </span>
+            </button>
+
+
+        </>
+    )
+}
