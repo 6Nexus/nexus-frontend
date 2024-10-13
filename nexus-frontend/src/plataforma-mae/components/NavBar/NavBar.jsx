0baa1a42a85bdc2e397f6795090f9c23b619f087
maria.viegas <maria.viegas@sptech.school>
2024-10-13 13:59:28 -0300

    Update: Adição de hover em botões, box-shadow nos cards de curso e resolução do bug na navbar

--- nexus-frontend/src/plataforma-mae/components/NavBar/NavBar.jsx
+++ nexus-frontend/src/plataforma-mae/components/NavBar/NavBar.jsx
@@ -11,95 +11,97 @@ import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LogoutIcon from '@mui/icons-material/Logout';
 
-import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const NavBar = () => {
     const [isExpanded, setExpanded] = useState(false);
-    const [activeItem, setActiveItem] = useState('home');
+    const [activeItem, setActiveItem] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();  
 
     const toggleMenu = () => {
         setExpanded(!isExpanded);
-    }
-
-
-    const navigate = useNavigate();
-
-    const goToHome = () => {
-        setActiveItem('home'); 
-        navigate('/');
     };
 
-    const goToCourse = () => {
-        setActiveItem('courses'); 
-        navigate('/cursos');
+    useEffect(() => {
+        const currentPath = location.pathname;
+        if (currentPath === '/') {
+            setActiveItem('home');
+        } 
+        
+        if (currentPath === '/cursos') {
+            setActiveItem('courses');
+        } 
+        
+        if (currentPath === '/instrucoes') {
+            setActiveItem('instructions');
+        } 
+        
+        if (currentPath === '/perfil') {
+            setActiveItem('profileSettings');
+        } 
+        
+        if (currentPath === '/certificados') {
+            setActiveItem('certificates');
+        } 
+        
+        if (currentPath === '/cursos-salvos') {
+            setActiveItem('saved');
+        }
+    }, [location]);
+
+    const handleNavigation = (item, route) => {
+        setActiveItem(item);
+        navigate(route);
     };
-
-    const goToInstructions = () => {
-        setActiveItem('instructions'); 
-        navigate('/instrucoes')
-    }
-
-    const goToProfileSettings = () => {
-        setActiveItem('profileSettings'); 
-        navigate('/perfil')
-    }
-
-    const goToCertificates = () => {
-        setActiveItem('certificates'); 
-        navigate('/certificados')
-    }
-
-    const goToSavedCourses = () => {
-        setActiveItem('saved'); 
-        navigate('/cursos-salvos')
-    }
 
     return (
         <>
             <nav className={`${styles.navbar} ${isExpanded ? styles.expanded : ''}`} aria-label="Menu de opções da plataforma">
-                <div class="menu__menu-icon" className={styles["menuIcon"]} onClick={toggleMenu}>
+                <div className={styles["menuIcon"]} onClick={toggleMenu}>
                     <MenuIcon sx={{ fontSize: 32, color: '#fff'}}/>
                 </div>
 
-                <div class="menu__menu-icon-options" className={styles["menuOptions"]}>
-                    <div class="menu-icon-options__primary" className={styles["menuOptions__primary"]}>
-                        <div className={styles["menuOptions__item"]} onClick={goToHome}>
+                <div className={styles["menuOptions"]}>
+                    <div className={styles["menuOptions__primary"]}>
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('home', '/')}>
                             <HomeIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'home' ? styles["active"] : ""}`}/>
                             {isExpanded && <span className={styles["menuOptions__item__text"]}>Início</span>}
                         </div>
 
-                        <div className={styles["menuOptions__item"]} onClick={goToCourse}>
-                                <SchoolIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'courses' ? styles["active"] : ""}`}/>
-                                {isExpanded && <span className={styles["menuOptions__item__text"]}>Cursos</span>}
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('courses', '/cursos')}>
+                            <SchoolIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'courses' ? styles["active"] : ""}`}/>
+                            {isExpanded && <span className={styles["menuOptions__item__text"]}>Cursos</span>}
                         </div>
 
-                        <div className={styles["menuOptions__item"]} onClick={goToInstructions}>
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('instructions', '/instrucoes')}>
                             <InfoIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'instructions' ? styles["active"] : ""}`}/>
                             {isExpanded && <span className={styles["menuOptions__item__text"]}>Instruções</span>}
                         </div>
 
-                        <div className={styles["menuOptions__item"]} onClick={goToProfileSettings}>
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('profileSettings', '/perfil')}>
                             <AccountCircleIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'profileSettings' ? styles["active"] : ""}`}/>
                             {isExpanded && <span className={styles["menuOptions__item__text"]}>Seu perfil</span>}
                         </div>
                     </div>
 
                     <div className={styles["menuOptions__line"]}></div>
-                        
-                    <div class="menu-icon-options__secondary" className={styles["menuOptions__secondary"]}>
-                        <div className={styles["menuOptions__item"]} onClick={goToCertificates}>
+
+                    <div className={styles["menuOptions__secondary"]}>
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('certificates', '/certificados')}>
                             <WorkspacePremiumIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'certificates' ? styles["active"] : ""}`}/>
                             {isExpanded && <span className={styles["menuOptions__item__text"]}>Certificados</span>}
                         </div>
-                        <div className={styles["menuOptions__item"]} onClick={goToSavedCourses}>
+
+                        <div className={styles["menuOptions__item"]} onClick={() => handleNavigation('saved', '/cursos-salvos')}>
                             <FavoriteIcon sx={{ fontSize: 32, color: '#fff'}} className={`${activeItem === 'saved' ? styles["active"] : ""}`}/>
                             {isExpanded && <span className={styles["menuOptions__item__text"]}>Salvos</span>}
                         </div>
                     </div>
                 </div>
 
-                <div class="menu__menu-icon-logout" className={styles["menuIconLogout"]}>
+                <div className={styles["menuIconLogout"]}>
                     <div className={styles["menuOptions__item"]}>
                         <LogoutIcon sx={{ fontSize: 32, color: '#fff'}}/>
                         {isExpanded && <span className={styles["menuOptions__item__text"]}>Sair</span>}
@@ -109,4 +111,5 @@ const NavBar = () => {
         </>
     );
 };
+
 export default NavBar;
--- nexus-frontend/src/plataforma-mae/components/SearchBar/SearchBar.jsx
+++ nexus-frontend/src/plataforma-mae/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,27 @@
 import * as React from "react";
 import styles from './SearchBar.module.css'
+import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
 import Logotipo from '../../../utils/assets/logotipo.svg'
 import TextIncreaseIcon from '@mui/icons-material/TextIncrease';
 import ContrastIcon from '@mui/icons-material/Contrast';
+import { useNavigate } from 'react-router-dom';
 
 
 const SearchBar = () => {
+
+    const [activeItem, setActiveItem] = useState('home');
+    const navigate = useNavigate();
+    const goToHome = () => {
+        setActiveItem('home'); 
+        navigate('/');
+    };
+
     return (
         <>
            <div className={styles["search-bar-container"]}>
-                <div className={styles["search-bar-container__logotipo"]}>
+                <div className={styles["search-bar-container__logotipo"]} onClick={goToHome}>
                    <img src={Logotipo} alt="" />
                 </div>
 
