ffb8c54daf990d1a2c99a221d25d170fea21ada9
eduardaguardiao <maria.guardiao@sptech.school>
2024-10-17 13:18:48 -0300

    ADD:função para passar caminho no icones da tela de perfil

--- nexus-frontend/src/plataforma-professor/componentes/NavBar/NavBar.jsx
+++ nexus-frontend/src/plataforma-professor/componentes/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import { useNavigate} from 'react-router-dom';
 import './NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
@@ -8,12 +9,22 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 
 function NavBar() {
+
+    const [activeItem, setActiveItem] = useState('');
+    const navigate = useNavigate();
+
+    const handleNavigation = (item, route) => {
+        setActiveItem(item);
+        navigate(route);
+    };
+
+
     return (
         <>
             <header>
                 <span>Meu Perfil</span>
                 <nav>
-                    <button className="icon-sair">
+                    <button className="icon-sair" onClick={() => handleNavigation('', '/curso-setup')}>
                         <FontAwesomeIcon icon={faHome} />
                     </button>
                 </nav>
