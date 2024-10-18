ccedcd102d0c77daedd0aa4a90b69e616c06a371
maria.viegas <maria.viegas@sptech.school>
2024-10-18 17:35:17 -0300

    Add: tela de questionário

--- nexus-frontend/src/routes.js
+++ nexus-frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./plataforma-mae/pages/Home/Home";
 import Course from "./plataforma-mae/pages/Course/Course";
 import CourseDetails from './plataforma-mae/pages/Course/CourseDetails'
+import Questionnaire from "./plataforma-mae/pages/Questionnaire/Questionnaire";
 import Instructions from "./plataforma-mae/pages/Instructions/Instructions";
 import ProfileSettings from "./plataforma-mae/pages/ProfileSettings/ProfileSettings";
 import Certificates from "./plataforma-mae/pages/Certificates/Certificates";
@@ -16,6 +17,7 @@ function Rotas() {
                 <Route path="/" element={<Home />} />
                 <Route path="/cursos" element={<Course />} />
                 <Route path="/cursos/:idCurso" element={<CourseDetails />} />
+                <Route path="/questionario/:idCurso" element={<Questionnaire />} />
                 <Route path="/instrucoes" element={<Instructions />} />
                 <Route path="/perfil" element={<ProfileSettings />} />
                 <Route path="/certificados" element={<Certificates />} />
