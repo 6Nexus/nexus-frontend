ec57406d2d6a2f55cd887ff3f2973e2812b7af95
maria.viegas <maria.viegas@sptech.school>
2024-10-19 22:38:32 -0300

    Add: Requisição no mockAPI e paginação na listagem de cursos

--- nexus-frontend/src/plataforma-mae/pages/SavedCourses/SavedCourses.jsx
+++ nexus-frontend/src/plataforma-mae/pages/SavedCourses/SavedCourses.jsx
@@ -1,29 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../../api.js";
 import styles from './SavedCourses.module.css'
 import SideBar from "../../components/SideBar/SideBar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import CardCurso from "../../components/CardCurso/CardCurso";
 import HeaderCategory from "../../components/HeaderCategory/HeaderCategory";
+import Pagination from '@mui/material/Pagination';
 
 const SavedCourses = () => {
+    const [cardsData, setCardsData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const cardsPerPage = 6;
+
+
+
+    function buscarCursos() {
+        api.get().then((response) => {
+            const { data } = response;
+            console.log(data);
+            setCardsData(data)
+
+        }).catch((e) => {
+            console.log("Deu erro", e)
+        })
+    }
+
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = cardsData.slice(indexOfFirstCard, indexOfLastCard);
+
+    const handleChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
+    useEffect(() => {
+        buscarCursos();
+    }, [])
     return (
         <>
             <div className={styles["savedCourses-container"]}>
-                <SideBar backgroundColor={'#245024'}/>
+                <SideBar backgroundColor={'#245024'} />
 
                 <div className={styles["savedCourses-container__content"]}>
                     <SearchBar />
-                    
-                    <HeaderCategory/>
-                
+
+                    <HeaderCategory />
+
                     <div className={styles["savedCourses-container__content__savedCoursesList"]}>
                         <p className={styles["savedCoursesList__title"]}>Cursos encontrados</p>
                         <div className={styles["savedCoursesList__cards"]}>
-                            <CardCurso title="Culinária brasiliera" subtitle="Modulo 1: Primeiro livro de receitas" textButton="Ver curso" inProgress={false}/>
-                            <CardCurso title="Automaquiagem" subtitle="Modulo 1: Maquiagem dia-a-dia" textButton="Ver curso" inProgress={false}/>
-                            <CardCurso title="Informática básica" subtitle="Modulo 1: Pacote office" textButton="Continuar" inProgress={true}/>
+                            {currentCards && currentCards.map((data, _) => (
+
+                                <CardCurso
+                                    key={data.idModulo}
+                                    title={data.titulo}
+                                    subtitle={data.descricao}
+                                    category={data.categoria}
+                                    inProgress={data.emProgresso}
+                                    liked={data.curtido}
+                                    imageUrl={data.imagem}
+                                    progress={data.progreso}
+                                />
+
+                            ))}
                         </div>
                     </div>
+                    <Pagination
+                        count={Math.ceil(cardsData.length / cardsPerPage)}
+                        page={currentPage} 
+                        onChange={handleChange}
+                        variant="outlined"
+                        shape="rounded"
+                        sx={{
+
+                            backgroundColor: '#F3F3F3',
+                            padding: '8px',
+                            borderRadius: '30px',
+                            '& .MuiPaginationItem-root': {
+                                color: '#245024', 
+                                border: 'none',
+                                borderRadius: '50%',
+                                fontSize:'16px'
+                            },
+                            '& .MuiPaginationItem-root.Mui-selected': {
+                                backgroundColor: '#3B9D3B', 
+                                color: 'white',
+                            },
+                        }}
+                    />
                 </div>
             </div>
         </>
