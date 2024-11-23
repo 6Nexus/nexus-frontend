703bb2ea24091d78048a56ce8e1587023a489d43
maria.viegas <maria.veigas@sptech.school>
2024-11-23 00:32:38 -0300

    Add: Exibe perguntas do questionário relacionado ao módulo e faz lógica de checar perguntas respondidas

--- nexus-frontend/src/index.js
+++ nexus-frontend/src/index.js
@@ -1,14 +1,5 @@
-import Routes from './routes';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
-import Login from './plataforma-professor/paginas/Login/Login';
-// import Cadastro from './plataforma-professor/paginas/Cadastro/Cadastro';
-import Perfil from './plataforma-professor/paginas/Perfil/Perfil';
-
-
-
 import Rotas from './routes'; 
 import './global.css';
 
@@ -16,9 +7,7 @@ import './global.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-
      <Rotas /> 
-
   </React.StrictMode>
 );
 
