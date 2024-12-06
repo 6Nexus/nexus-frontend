c03fab73f92969d87cf6211e4b58d5716a01f70f
maria.viegas <maria.viegas@sptech.school>
2024-12-06 01:50:51 -0300

    Add: Função para buscar vídeos de um módulo

--- nexus-frontend/src/plataforma-mae/components/PlaylistYt/YoutubePlaylist.jsx
+++ nexus-frontend/src/plataforma-mae/components/PlaylistYt/YoutubePlaylist.jsx
@@ -1,34 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import api from '../../../api'
 import styles from './YoutubePlaylist.module.css';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import IconCourse from '../../../utils/assets/course.svg';
 import ButtonMaterial from '../ButtonMaterial/ButtonMaterial';
 
 const YoutubePlaylist = ({ titlePlaylist, playlistId, isCursoDetails }) => {
+  const { idModule } = useParams();
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [loading, setLoading] = useState(true);
-  const API_KEY = 'AIzaSyBWPdWoACygsmbt5jhGJ1ff-DWBV_8ojUY';
 
   useEffect(() => {
     const fetchVideos = async () => {
-      console.log(playlistId)
       try {
-        const res = await axios.get(
-          `https://www.googleapis.com/youtube/v3/playlistItems`,
-          {
-            params: {
-              part: 'snippet',
-              playlistId: playlistId,
-              maxResults: 50,
-              key: API_KEY,
-            }
-          }
-        );
-        if (res.data.items) {
-          setVideos(res.data.items);
-          setSelectedVideo(res.data.items[0]);
+        const res = await api.get(`/videos/modulo/${idModule}`);
+        if (res.data) {
+          setVideos(res.data);
+          setSelectedVideo(res.data[0]);
         }
       } catch (error) {
         console.error('Erro ao buscar videos:', error);
@@ -39,7 +29,7 @@ const YoutubePlaylist = ({ titlePlaylist, playlistId, isCursoDetails }) => {
 
     fetchVideos();
 
-  }, [playlistId]);
+  }, []);
 
   if (loading) {
     return <p>Carregando aulas...</p>;
@@ -49,14 +39,14 @@ const YoutubePlaylist = ({ titlePlaylist, playlistId, isCursoDetails }) => {
     <div className={styles['playlist-container']}>
       {selectedVideo && (
         <div className={styles['playlist-container__selectedVideo']}>
-          <h2 className={styles['selectedVideo__title']}>{selectedVideo.snippet.title}</h2>
+          <h2 className={styles['selectedVideo__title']}>{selectedVideo.titulo}</h2>
           <iframe
             width="100%"
             height="800"
-            src={`https://www.youtube.com/embed/${selectedVideo.snippet.resourceId.videoId}`}
+            src={`https://www.youtube.com/embed/${selectedVideo.youtubeUrl}`}
             frameBorder="0"
             allowFullScreen
-            title={selectedVideo.snippet.title}
+            title={selectedVideo.titulo}
             style={{ objectFit: 'cover' }}
           ></iframe>
           {
@@ -72,17 +62,17 @@ const YoutubePlaylist = ({ titlePlaylist, playlistId, isCursoDetails }) => {
         <h2 className={styles['videos__title']}>{titlePlaylist}</h2>
         <div className={styles['videos__container']}>
           {videos.map((video) => {
-            const thumbnailUrl = video.snippet.thumbnails?.medium?.url;
+            const thumbnailUrl = null;
             return (
               <div
-                key={video.snippet.resourceId.videoId}
+                key={video.id}
                 className={styles['videos__container__item']}
               >
                 <div className={styles['item__thumbnail']}>
                   {thumbnailUrl ? (
                     <img
                       src={thumbnailUrl}
-                      alt={video.snippet.title}
+                      alt={video.titulo}
                       onClick={(e) => {
                         e.stopPropagation();
                         setSelectedVideo(video);
@@ -90,10 +80,18 @@ const YoutubePlaylist = ({ titlePlaylist, playlistId, isCursoDetails }) => {
                     />
                   ) : (
                     <div className={styles['item__thumbnail__secondary']}>
-                      <img src={IconCourse} alt="" />
+                      <img
+                        src={IconCourse}
+                        alt={video.titulo}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setSelectedVideo(video);
+                        }}
+
+                      />
                     </div>
                   )}
-                  <p className={styles['item__thumbnail__title']}>{video.snippet.title}</p>
+                  <p className={styles['item__thumbnail__title']}>{video.titulo}</p>
                 </div>
 
                 {
