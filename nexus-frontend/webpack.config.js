2710e211742adccb6be3a6e819c7c221ee73fc54
mariaeviegas <maria.viegas@sptech.school>
2025-04-27 18:23:32 -0300

    Add: configuração webpack

---
+++ nexus-frontend/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/', 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'), 
+    compress: true, 
+    port: 3000, 
+    historyApiFallback: true
+  },
+};
